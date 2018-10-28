ca2327c090c8a446b90649fcd6624f5df951dc1c
Christina-Ilyushonok <iluyshonok.kristina@gmail.com>
2018-10-28 20:28:56 +0300

    feat: add task3

---
+++ src/recursion.js
@@ -0,0 +1,18 @@
+function recursion(node, arr, depth) {
+    arr = arr || [];
+    depth = depth || 0;
+    let values = arr[depth];
+    if (values) {
+        values.push(node.value);
+    } else {
+        arr[depth] = [node.value];
+    }
+    depth++;
+    if (node.left) {
+        recursion(node.left, arr, depth);
+    }
+    if (node.right) {
+        recursion(node.right, arr, depth);
+    }
+    return arr;
+}
