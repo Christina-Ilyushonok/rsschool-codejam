d803aeadb95a4554e41473396ab5dabf0747cb3c
Christina-Ilyushonok <iluyshonok.kristina@gmail.com>
2018-10-28 20:28:18 +0300

    feat: add task2

---
+++ src/make.js
@@ -0,0 +1,19 @@
+function make() {
+    let numbers = [];
+    function f() {
+        let args = [...arguments];
+        for (let i = 0; i < args.length; i++) {
+            if ('function' === typeof args[i]) {
+                return numbers.reduce(args[i]);
+            } else {
+                numbers.push(args[i]);
+            }
+        }
+        return f;
+    }
+    return f(...arguments);
+}
+
+function sum(a, b) {
+    return a + b;
+};
